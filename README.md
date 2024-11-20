dcc048a96479270ec8cc5a6fa0abb2d062e9b3fd
ian-ledig <ian.ledigfr@gmail.com>
2024-11-20 18:41:25 +0100

    adding different kanji reading and level selection in settings

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ Kanji Test is an interactive application designed to help users learn and memori
 - **Translation Option**: You can choose to enable or disable the translation feature, tailoring the experience to your preference or learning needs.
 - **Intelligent Error Correction**: The app features an intelligent system that highlights and revisits specific kanji or kana you frequently make mistakes on, helping you strengthen your weak points.
 - **Simple and Intuitive Interface**: The app has a clean, user-friendly interface to focus solely on learning kanji.
+- **Select Kanji Readings**: You can choose whether to focus on Sino-Japanese (音読み, on'yomi) readings or native Japanese (訓読み, kun'yomi) readings, tailoring the experience to your learning preferences.
+- **Kanji Level Selection**: You can select the level of kanji from JLPT levels 5 to 1, allowing you to practice kanji at your own proficiency level.
 
 Random Kanji Display: The app shows random kanji characters from a list, making it easy to practice new ones regularly.
 Kana Input: Test your knowledge by typing the correct kana for each displayed kanji.
--- renderer.js
+++ renderer.js
@@ -1,37 +1,53 @@
 const fs = require('fs');
 const path = require('path');
 
-const kanjiFilePath = path.join(__dirname, 'assets', 'kanji.txt');
-const kanjiList = fs.readFileSync(kanjiFilePath, 'utf-8')
-  .split('\n')
-  .filter(line => line.trim())
-  .map(line => {
-    const [kanji, kana, translation] = line.split('　');
-    return { kanji, kana, translation };
-  });
+const kanjiFilePath = path.join(__dirname, 'assets', 'kanji-jouyou.json');
+const kanjiData = JSON.parse(fs.readFileSync(kanjiFilePath, 'utf-8'));
 
+let kanjiList = [];
 let count = 0;
 let currentKanji = {};
 let errors = {};
+let kun = true;
+let level = 5;
+
+function loadKanjiList(level, kun) {
+  kanjiList = [];
+  errors = {};
+  
+  document.getElementById('kanji').textContent = '';
+  document.getElementById('translation').textContent = '';
+  document.getElementById('log').textContent = '';
+
+  kanjiList = Object.entries(kanjiData)
+    .filter(([_, details]) => details.jlpt_new === level)
+    .map(([kanji, details]) => ({
+      kanji,
+      kana: kun 
+        ? (details.readings_kun.length > 0 ? details.readings_kun[0] : details.readings_on[0])
+        : (details.readings_on.length > 0 ? details.readings_on[0] : details.readings_kun[0]),
+      translation: details.meanings[0],
+    }));
+}
 
 function loadRandomKanji() {
-  if(count % 3 === 1 && errors.length !== 0){
-    // review previous mistakes
-    const errorKanji = Object.keys(errors).reduce((a, b) => errors[a] > errors[b] ? a : b);
+  const mainPage = document.getElementById('main-page');
+
+  if (count % 3 === 1 && Object.keys(errors).length !== 0) {
+    // review mistakes
+    const errorKanji = Object.keys(errors).reduce((a, b) =>
+      errors[a] > errors[b] ? a : b
+    );
     currentKanji = kanjiList.find(kanji => kanji.kanji === errorKanji);
 
     errors[errorKanji] = Math.max(0, errors[errorKanji] - 1);
     if (errors[errorKanji] === 0) {
       delete errors[errorKanji];
     }
-
-    const mainPage = document.getElementById('main-page');
     mainPage.style.backgroundColor = '#fcf3cf';
   } else {
     // load random kanji
     currentKanji = kanjiList[Math.floor(Math.random() * kanjiList.length)];
-
-    const mainPage = document.getElementById('main-page');
     mainPage.style.backgroundColor = '#F9F6EE';
   }
 
@@ -43,40 +59,59 @@ document.getElementById('close').addEventListener('click', () => {
   window.close();
 });
 
-document.getElementById('input').addEventListener('keydown', (event) => {
+document.getElementById('kana-input').addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
-    const userInput = document.getElementById('input').value.trim();
-    
-    if (userInput.localeCompare(currentKanji.kana, undefined, { sensitivity: 'base' }) === 0) {
+    const userInput = document.getElementById('kana-input').value.trim();
+
+    const cleanedKana = currentKanji.kana.replace(/[-.]/g, '');
+    if (
+      userInput.localeCompare(
+        cleanedKana,
+        undefined,
+        { sensitivity: 'base' }
+      ) === 0
+    ) {
       document.getElementById('log').textContent = '';
       loadRandomKanji();
 
       count++;
       document.getElementById('count').textContent = count;
     } else {
-      errors[currentKanji.kanji] = errors[currentKanji.kanji] + 3;
+      errors[currentKanji.kanji] = (errors[currentKanji.kanji] || 0) + 3;
       document.getElementById('log').textContent = `Correction: ${currentKanji.kana}`;
     }
-    document.getElementById('input').value = '';
+    document.getElementById('kana-input').value = '';
   }
 });
 
 document.getElementById('settings').addEventListener('click', (event) => {
   const mainPage = document.getElementById('main-page');
-  const SettingsPage = document.getElementById('settings-page');
+  const settingsPage = document.getElementById('settings-page');
 
-  if(mainPage.style.display == 'none'){
+  if (mainPage.style.display == 'none') {
     mainPage.style.display = 'flex';
-    SettingsPage.style.display = 'none';
+    settingsPage.style.display = 'none';
   } else {
     mainPage.style.display = 'none';
-    SettingsPage.style.display = 'flex';
+    settingsPage.style.display = 'flex';
   }
 });
 
+// settings
 document.getElementById('toggle-translation').addEventListener('change', (event) => {
   const translationDiv = document.getElementById('translation');
   translationDiv.style.display = event.target.checked ? 'flex' : 'none';
 });
+document.getElementById('toggle-kun').addEventListener('change', (event) => {
+  kun = !event.target.checked;
+  loadKanjiList(level, kun);
+  loadRandomKanji();
+});
+document.getElementById('setting-input').addEventListener('change', (event) => {
+  level = parseInt(event.target.value);
+  loadKanjiList(level, kun);
+  loadRandomKanji();
+});
 
+loadKanjiList(5, true);
 loadRandomKanji();
